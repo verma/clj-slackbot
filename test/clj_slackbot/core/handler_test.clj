d101e6e22dfc27af25e734bf6040acd2b459e261
Shashy Dass <ShashyD@gmail.com>
2015-09-25 07:59:55 -0400

    Removing tests - they don't do anything

--- test/clj_slackbot/core/handler_test.clj
+++ test/clj_slackbot/core/handler_test.clj
@@ -3,12 +3,3 @@
             [ring.mock.request :as mock]
             [clj-slackbot.core.handler :refer :all]))
 
-(deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
