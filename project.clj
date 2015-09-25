5f666efc88bbdc58ef545b40c6cc8a25c9e81cde
Shashy Dass <ShashyD@gmail.com>
2015-09-25 07:19:51 -0400

    Bump dependencies

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
                  [clojail "1.0.6"]
-                 [clj-http "1.0.1"]
-                 [cheshire "5.3.1"]
-                 [environ "1.0.0"]
-                 [ring/ring-jetty-adapter "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
+                 [clj-http "2.0.0"]
+                 [cheshire "5.5.0"]
+                 [environ "1.0.1"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]]
+  :plugins [[lein-ring "0.9.6"]]
   :ring {:handler clj-slackbot.core.handler/app}
   :uberjar-name "clj-slackbot.jar"
   :main clj-slackbot.core.handler
