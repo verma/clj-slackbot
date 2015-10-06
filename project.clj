6b232fe6a692f753ff0a8e251ab496576bd1bf2f
Christophe De Troyer <christophe.detroyer@gmail.com>
2015-10-06 15:58:26 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [compojure "1.2.0"]
+                 [compojure "1.4.0"]
                  [clojail "1.0.6"]
-                 [clj-http "1.0.1"]
-                 [cheshire "5.3.1"]
-                 [environ "1.0.0"]
-                 [stylefruits/gniazdo "0.4.0"]
-                 [ring/ring-jetty-adapter "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [clj-http "2.0.0"]
+                 [cheshire "5.5.0"]
+                 [environ "1.0.1"]
+                 [stylefruits/gniazdo "0.4.1" ]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]]
   :plugins [[lein-ring "0.8.13"]]
   :uberjar-name "clj-slackbot.jar"
   :main clj-slackbot.core
