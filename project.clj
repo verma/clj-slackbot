9aedb794d3abe4ad4b05686fc669a20a602cf4b9
Cliff Rodgers <kliph@users.noreply.github.com>
2016-08-07 15:28:52 -0500

    Update dependencies

--- project.clj
+++ project.clj
@@ -2,21 +2,19 @@
   :description "A slackbot for managing our grocery list"
   :url "https://github.com/kliph/mog-clj-slackbot"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [compojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                 [org.clojure/core.async "0.2.385"]
+                 [compojure "1.5.1"]
                  [clojail "1.0.6"]
-                 [clj-http "1.0.1"]
-                 [cheshire "5.3.1"]
-                 [environ "1.0.0"]
-                 [stylefruits/gniazdo "0.4.1"]
-                 [ring/ring-defaults "0.1.5"]
+                 [clj-http "2.2.0"]
+                 [cheshire "5.6.3"]
+                 [environ "1.1.0"]
+                 [stylefruits/gniazdo "1.0.0"]
+                 [ring/ring-defaults "0.2.1"]
                  [http-kit "2.1.18"]]
-  :plugins [[lein-ring "0.8.13"]]
+  :plugins [[lein-ring "0.9.7"]]
   :uberjar-name "mog.jar"
   :main mog.core
   :profiles
-  {:dev {:repl-options {:init-ns mog.core.handler}
-         :dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}
+  {:dev {:repl-options {:init-ns mog.core.handler}}
    :uberjar {:aot :all}})
