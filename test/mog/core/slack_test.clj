c4579f5b7eea153da2d6fc3c48bd826d4f176848
Cliff Rodgers <kliph@users.noreply.github.com>
2016-08-08 21:39:54 -0500

    Add test for pass-through command behavior

--- test/mog/core/slack_test.clj
+++ test/mog/core/slack_test.clj
@@ -1,6 +1,21 @@
 (ns mog.core.slack-test
-  (:require [clojure.test :refer [deftest testing is]]))
+  (:require [clojure.core.async :as async]
+            [clojure.test :refer [deftest testing is]]
+            [mog.core :as core]))
 
-(deftest test-setup
-  (testing "test setup"
-    (is (= 1 1))))
+(defn mock-command-loop []
+  (let [in (async/chan)
+        out (async/chan)
+        stop (fn []
+               (async/close! in)
+               (async/close! out))]
+    (core/command-loop [in out stop])
+    [in out stop]))
+
+(deftest command-loop
+  (testing "pass through"
+    (let [[in-chan out-chan stop-fn] (mock-command-loop)
+          input {:input "test"}
+          output (do (async/>!! in-chan input)
+                     (async/<!! out-chan))]
+      (is (= (:input input) (:mog/response output))))))
